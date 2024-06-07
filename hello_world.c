1e8e26674b45e1a354822299d7321da729306357
cRefused <itaos@yandex.ru>
2024-06-07 16:18:57 +0500

    ...

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-// https://ru.wikibooks.org/wiki/%D0%AF%D0%B7%D1%8B%D0%BA_%D0%A1%D0%B8_%D0%B2_%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D0%B0%D1%85/%D0%9F%D1%80%D0%BE%D1%81%D1%82%D0%B5%D0%B9%D1%88%D0%B0%D1%8F_%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B0_%C2%ABHello_World%C2%BB
-
 int main(void)
 {
   puts("Hello, World!");

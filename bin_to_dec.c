32a97a289b408bf7fcf7d51cbfda49f82bcef106
cRefused <itaos@yandex.ru>
2024-06-11 08:15:31 +0500

    ...

--- bin_to_dec.c
+++ bin_to_dec.c
@@ -5,19 +5,21 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_BITS 64 // максимум бит, которые будем считать
+
 typedef unsigned long long int INT64U;
 
-const INT64U maxNumX64 = 9223372036854775807ull; // ограничения для 64 бит
- 
+const INT64U max_num_x64 = 9223372036854775807ull; // ограничения для 64 бит
+
 // возведение числа в степень
 INT64U fnExp(int n, int e)
 {
   INT64U result = 1;
   for(int i = 0; i < e; i++)
   {
-    if(result * n >= maxNumX64)
+    if(result * n >= max_num_x64)
     {
-      return maxNumX64;
+      return max_num_x64;
     }
     else
     {
@@ -28,41 +30,44 @@ INT64U fnExp(int n, int e)
 }
 
 // преобразование двоичного в десятичное
-void fnBinaryCode(INT64U * res)
+void fn_binary_code(char *str_bin, INT64U *res)
 {
-  const unsigned int maxBits = 64; // максимум бит, которые будем считать
   INT64U r = 0; // результат (10-е число)
-  char strBin[maxBits]; // тут будет введенная строка
 
-  printf("Введите двоичное число (максимум %d \bbit): ", maxBits);
-  scanf("%s", strBin); // передаем строку в массив
+  printf("Введите двоичное число (максимум %d \bbit): ", MAX_BITS);
+  fgets(str_bin, MAX_BITS + 1, stdin); // передаем строку в массив
 
-  int lenBits = strlen(strBin); // длина строки
-  int countBits = (lenBits < maxBits ? lenBits : maxBits); // сколько бит считать (если строка короче maxBits)
-  int endBit = (lenBits < maxBits ? 0 : lenBits - maxBits); // до какого бита идти (если строка длиннее maxBits)
+  int len_bits = strlen(str_bin); // длина строки
+  int count_bits = (len_bits < MAX_BITS ? len_bits : MAX_BITS); // сколько бит считать (если строка короче MAX_BITS)
+  int end_bit = (len_bits < MAX_BITS ? 0 : len_bits - MAX_BITS); // до какого бита идти (если строка длиннее MAX_BITS)
 
   // проходимся по символам с конца
-  for (int i = lenBits - 1; i >= endBit; i--)
+  for (int i = len_bits - 1; i >= end_bit; i--)
   {
-    int deg = lenBits - i - 1; // в какую степень возводить
-    
+    int deg = len_bits - i - 1; // в какую степень возводить
+
     // если '1', возводим в соотв. степень и плюсуем к результату
-    if(strBin[i] == '1')
+    if(str_bin[i] == '1')
     {
-      r = (r + fnExp(2, deg) >= maxNumX64 ? maxNumX64 : r + fnExp(2, deg));
+      r = (r + fnExp(2, deg) >= max_num_x64 ? max_num_x64 : r + fnExp(2, deg));
+    }
+    else if(str_bin[i] == 10)
+    {
+      str_bin[i] = '\0';
     }
   }
-  
+
   // результат
-  res[0] = countBits; // сколько бит
+  res[0] = count_bits; // сколько бит
   res[1] = r; // 10-е число
 }
 
 int main(void)
 {
-  INT64U res[] = {0, 0}; // массив значений (сколько бит, 10-е число)
-  fnBinaryCode(res); // вызываем считалку, которая заполнит наш массив
-  printf("Двоичное (%d \bbit), десятичное: %lld \n", res[0], res[1]);
+  char str_bin[MAX_BITS]; // тут будет введенная строка
+  INT64U res[2]; // массив значений (сколько бит, 10-е число)
+  fn_binary_code(str_bin, res); // вызываем считалку, которая заполнит наш массив
+  printf(" Двоичное: [%d \bbit][%s]\n Десятичное: %lld \n", res[0], str_bin, res[1]);
   return 0;
 }
 
